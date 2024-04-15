d6b887ae6a231aab9ebe5afea04f95eed98b07da
Harshit Parmar <parmarharshit2402@gmail.com>
2024-04-15 15:09:34 +0530

    Create Foodcost.java

---
+++ Foodcost.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int X = scanner.nextInt(); 
+        int Y = scanner.nextInt(); 
+
+        
+        int totalCost = X * 6 + Y;
+
+        System.out.println(totalCost);
+    }
+}
